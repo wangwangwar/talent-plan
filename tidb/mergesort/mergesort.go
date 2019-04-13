4882494b0f729342b36a187e85ee0469dd02ce61
wangwangwar <wangwangwar@gmail.com>
2019-04-13 11:56:45 +0800

    version2 - 去掉了大部分不必要的内存分配

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -3,39 +3,34 @@ package main
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
 func MergeSort(src []int64) {
-	dest := make([]int64, len(src))
-	copy(dest, src)
-
-	if len(dest) > 1 {
-		middle := len(dest) / 2
-		MergeSort(dest[:middle])
-		MergeSort(dest[middle:])
-		merge(&dest, middle)
+	if len(src) > 1 {
+		middle := len(src) / 2
+		MergeSort(src[:middle])
+		MergeSort(src[middle:])
+		merge(src, middle)
 	}
-
-	copy(src, dest)
 }
 
-func merge(src *[]int64, middle int) {
-	r := new([]int64)
+func merge(src []int64, middle int) {
+	r := make([]int64, 0, len(src))
 	i, j := 0, middle
-	for i < middle && j < len(*src) {
-		if (*src)[i] < (*src)[j] {
-			*r = append(*r, (*src)[i])
+	for i < middle && j < len(src) {
+		if src[i] < src[j] {
+			r = append(r, src[i])
 			i++
 		} else {
-			*r = append(*r, (*src)[j])
+			r = append(r, src[j])
 			j++
 		}
 	}
 
 	if i == middle {
-		*r = append(*r, (*src)[j:]...)
+		r = append(r, src[j:]...)
 	}
 
-	if j == len(*src) {
-		*r = append(*r, (*src)[i:middle]...)
+	if j == len(src) {
+		r = append(r, src[i:middle]...)
 	}
 
-	*src = *r
+	copy(src, r)
 }
